f2fcebd5caccc0a7fb52d7d0a7c353367c78b7bb
pruwt <prudencetheuri@gmail.com>
2023-11-09 12:41:41 +0300

    created lecturer dash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.uniapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.uniapp"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
