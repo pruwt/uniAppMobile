d3f7e2d0dd08d32054bcf9a3cf5506a0ed766c7b
Prisha1611 <prishapatel16@gmail.com>
2023-11-19 15:49:28 +0300

    "student part"

--- app/src/main/java/com/example/uniapp/MainActivity3.java
+++ app/src/main/java/com/example/uniapp/MainActivity3.java
@@ -2,13 +2,36 @@ package com.example.uniapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity3 extends AppCompatActivity {
+    Button button5, button6, button7, button8, button9, button10;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main6);
+        setContentView(R.layout.activity_main3);
+
+        button5 = findViewById(R.id.button5);
+        button6 = findViewById(R.id.button6);
+        button7 = findViewById(R.id.button7);
+        button8 = findViewById(R.id.button8);
+        button9 = findViewById(R.id.button9);
+        button10 = findViewById(R.id.button10);
+
+        button10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate back to the login page (MainActivity)
+                Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+                startActivity(intent);
+                // Finish the current activity to prevent going back to MainActivity2 when pressing the back button
+                finish();
+            }
+        });
     }
 }
