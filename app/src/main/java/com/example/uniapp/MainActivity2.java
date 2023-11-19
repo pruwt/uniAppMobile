fc6b811f17cf6bb1a964ea07f8c6f02fee1cc859
Prisha1611 <prishapatel16@gmail.com>
2023-11-19 14:12:18 +0300

    "student part"

--- app/src/main/java/com/example/uniapp/MainActivity2.java
+++ app/src/main/java/com/example/uniapp/MainActivity2.java
@@ -2,13 +2,66 @@ package com.example.uniapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    Button button, button2, button3, button4, buttonReport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main5);
+        setContentView(R.layout.activity_main2);
+
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        buttonReport = findViewById(R.id.buttonReport);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity3.class);
+                startActivity(intent);
+            }
+        });
+
+//        button2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(), MainActivity6.class);
+//                startActivity(intent);
+//            }
+//        });
+
+//        button3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(), MainActivity4.class);
+//                startActivity(intent);
+//            }
+//        });
+
+//        buttonReport.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(), MainActivity5.class);
+//                startActivity(intent);
+//            }
+//        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate back to the login page (MainActivity)
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                // Finish the current activity to prevent going back to MainActivity2 when pressing the back button
+                finish();
+            }
+        });
     }
 }
