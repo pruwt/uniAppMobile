de779f9ae55518c994264808ed158704f77d956b
Prisha1611 <prishapatel16@gmail.com>
2023-11-19 15:56:27 +0300

    "student part"

---
+++ app/src/main/java/com/example/uniapp/MainActivity7.java
@@ -0,0 +1,36 @@
+package com.example.uniapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+import android.os.Bundle;
+
+public class MainActivity7 extends AppCompatActivity {
+
+    Button btnRegister, btnSummary, btnLogout, btnReports, btnScores;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main7);
+
+        btnRegister = findViewById(R.id.btnRegister);
+        btnSummary = findViewById(R.id.btnSummary);
+        btnLogout = findViewById(R.id.btnLogout);
+        btnReports = findViewById(R.id.btnReports);
+        btnLogout = findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                // Finish the current activity to prevent going back to MainActivity2 when pressing the back button
+                finish();
+            }
+        });
+    }
+}
